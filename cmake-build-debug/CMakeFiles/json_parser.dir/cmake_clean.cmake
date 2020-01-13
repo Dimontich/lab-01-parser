cfcef78496b6e2d56f2531fb22e415bad8408830
dmitrij <dimasek28@gmail.com>
2020-01-13 12:54:53 +0300

    parser5

---
+++ cmake-build-debug/CMakeFiles/json_parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/json_parser.dir/sources/Json.cpp.o"
+  "libjson_parserd.a"
+  "libjson_parserd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/json_parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
